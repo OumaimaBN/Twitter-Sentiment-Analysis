b8b04702a74baffc0936f708265dc98864a9c91e
OumaimaBN <ob.bentakhou@gmail.com>
2022-05-23 12:25:23 +0200

    update kafkaTwitterStreaming script

--- kafkaTwitterStreaming.py
+++ kafkaTwitterStreaming.py
@@ -1,30 +1,26 @@
 # import necessary modules
 from tweepy import OAuthHandler
-from tweepy import Stream
 from tweepy.streaming import Stream
 from kafka import KafkaProducer
 
 import json
-import pykafka
-#from afinn import Afinn
-#from langdetect import detect
-from textblob import TextBlob
 import pickle
-# initialize twitter producer class, inheriting from StreamListener class
-# utilities
 import re
-#nltk
+# nltk
 import nltk
+
+# initializing Lemmatizer and stopwords
 from nltk.stem import WordNetLemmatizer
 from nltk.corpus import stopwords
+
 nltk.download('stopwords')
-#initializing Lemmatizer and stopwords
 lemma = WordNetLemmatizer()
 stop_words = stopwords.words("english")
+
+
 # define a function to clean the tweet.
 def clean_tweet(tweet):
-
-    '''
+    """
     tweet: String
            Input Data
     tweet: String
@@ -39,7 +35,7 @@ def clean_tweet(tweet):
           lemmatize, tokenize (nouns and verb), remove stop words, filter all the non-alphabetic words, then join
           them again
 
-    '''
+    """
 
     tweet = tweet.lower()
     tweet = re.sub('((www.[^s]+)|(https?://[^s]+))', ' ', tweet)
@@ -63,8 +59,6 @@ class TweetListener(Stream):
             send_data = '{}'
             json_send_data = json.loads(send_data)
 
-
-
             # make checks for retweet and extended tweet-->done for truncated text
             if "retweeted_status" in json_data:
                 try:
@@ -74,16 +68,16 @@ class TweetListener(Stream):
                     json_send_data['text'] = json_data['retweeted_status']['text']
             else:
                 try:
-                    json_send_data['text'] = json_data['extended_tweet']['full_text']  
+                    json_send_data['text'] = json_data['extended_tweet']['full_text']
                 except:
                     json_send_data['text'] = json_data['text']
 
             json_send_data['creation_datetime'] = json_data['created_at']
             json_send_data['username'] = json_data['user']['name']
-            json_send_data['location'] = json_data['user']['location'] 
-            json_send_data['userDescr'] = json_data['user']['description']            
+            json_send_data['location'] = json_data['user']['location']
+            json_send_data['userDescr'] = json_data['user']['description']
             json_send_data['followers'] = json_data['user']['followers_count']
-            json_send_data['retweets'] = json_data['retweet_count']            
+            json_send_data['retweets'] = json_data['retweet_count']
             json_send_data['favorites'] = json_data['favorite_count']
 
             # language detection and use of appropriate sentiment analysis module
@@ -92,14 +86,14 @@ class TweetListener(Stream):
             txt = " ".join(txt)
             print(txt)
             txt = vec.transform([txt])
-            json_send_data['senti_val']= str(LRm.predict(txt)[0])
+            json_send_data['senti_val'] = str(LRm.predict(txt)[0])
             print(json_send_data)
-            producer.send("twitterstream", bytes(json.dumps(json_send_data),'ascii'))
+            producer.send("twitterstream", bytes(json.dumps(json_send_data), 'ascii'))
 
             return True
         except KeyError:
             return True
-        
+
     def on_error(self, status):
         print(status)
         return True
@@ -123,5 +117,5 @@ auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
 
 # perform activities on stream
-twitter_stream = TweetListener( consumer_key, consumer_secret,access_token, access_secret)
+twitter_stream = TweetListener(consumer_key, consumer_secret, access_token, access_secret)
 twitter_stream.filter(track=words, languages=["en"])
